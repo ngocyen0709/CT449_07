651af92f9dff2f8d501b43f6529590883661e990
NgocYen <yen1906811@student.ctu.edu.vn>
2022-09-21 15:16:59 +0700

    cai dat express chay thong bao xin chao

---
+++ App/config/index.js
@@ -0,0 +1,11 @@
+const config = {
+    app: {
+    port: process.env.PORT || 3031,
+    }
+};
+    module.exports = config; 
+    
+    
+    // db: {
+    //         uri: process.env.MONGODB_URI || "mongodb://localhost:27017/contactbook"
+    //     }
