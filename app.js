df307d631c2d4c1122304636d716e37a48324f75
NgocYen <yen1906811@student.ctu.edu.vn>
2022-09-21 15:13:52 +0700

    cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+//const { BadRequestError, errorHandler } = require("./app/errors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/",(req, res) => {
+    res.json({message:"Welcome to contact book application."});
+});
+//const setupContactRoutes = require("./app/routes/contact.routes");
+   
+
+// setupContactRoutes(app);
+// app.use((req, res, next) => { next(new BadRequestError(404, "Resource not found"));
+//     });
+//     app.use((error, req, res, next) => {
+//      errorHandler.handleError(error, res);
+//     });
+module.exports =app;
